c748f85e4d6ed269e6346fe284d3a0cb1e3ff795
Wecreds <Wecredsbr@gmail.com>
2024-11-01 10:19:14 -0300

    feat: adicionando botão de voltar para home na página de login

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,8 +1,15 @@
 <template>
+    <div class="fixed bottom-5 left-5 p-0 cursor-pointer">
+    <RouterLink to="/">
+      <HomeIcon class="w-6 h-6 text-rich-black" />
+    </RouterLink>
+  </div>
   <div class="flex min-h-screen justify-center items-center px-6 py-12 lg:px-8 text-rich-black">
     <LoginComponent />
   </div>
 </template>
 <script setup>
+import { RouterLink } from 'vue-router';
+import { HomeIcon } from '@heroicons/vue/24/solid'
 import LoginComponent from '@/components/LoginComponent.vue'
 </script>
